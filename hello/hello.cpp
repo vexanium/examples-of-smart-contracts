a31a7ff6e4cca56f0e84b046d52adbf4c3d3b219
Yuda Adi Pratama <61916091+YudaAdiPratama@users.noreply.github.com>
2022-06-13 13:38:29 +0700

    Update hello.cpp

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,1 +1,8 @@
-
+#include <eosio/eosio.hpp>
+class [[eosio::contract]] hello : public eosio::contract {
+  public:
+      using eosio::contract::contract;
+      [[eosio::action]] void hi( eosio::name user ) {
+         print( "Hello, ", user);
+      }
+};
